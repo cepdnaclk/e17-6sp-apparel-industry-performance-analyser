49f20a68a89a115d63564503dd63cf80b72fa8c7
Udith <uaudith@gmail.com>
2022-03-05 22:41:44 +0530

    NEW: create appbar fixes AB#10

--- src/front-end/src/App.tsx
+++ src/front-end/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonAppBar from "./components/appbar";
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
