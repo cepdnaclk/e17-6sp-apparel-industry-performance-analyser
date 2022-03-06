b0a10eecd50ebd70abdcdf0b3968439ab77ab04a
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2022-03-06 08:35:00 +0530

    NEW: Implement kpi-time graph with mock data

--- src/front-end/src/App.tsx
+++ src/front-end/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import KpiVsTime from './components/graphs/kpi/KpiVsTime';
+
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "test-graph">
+      <KpiVsTime xData={['jan','feb','mar','apr']} yData={['1','7','3','5']} title = {"On time delivery rate"}/>
     </div>
   );
 }
